ffa41a2ab5073422f84ef8c72741ee9696c70985
madhuri maniktahla <madhuri.maniktahla@gmail.com>
2023-09-20 14:00:16 +1000

    Extract data from API

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+const apiKey="9c94b7a29671191c02aa22567b99af7f";
 const baseUrl = 'https://api.themoviedb.org/3/';
 const moviesUrl = `${baseUrl}discover/movie?sort_by=popularity.desc&api_key=${apiKey}`;
 const trendingUrl = `${baseUrl}trending/all/day?language=en-US&api_key=${apiKey}`;
+const imgUrl = 'https://image.tmdb.org/t/p/w500';
 
 const main = document.getElementById('main');
 const trending = document.querySelector('.trending');
@@ -13,4 +15,21 @@ async function getMovies(url) {
         const response = await fetch(url);
         const data = await response.json();
         console.log(data.results);
+
+        data.results.forEach(element => {
+            const { poster_path,title, name, vote_average, id } = element;
+            
+            const movieCard = document.createElement('div');
+            movieCard.classList.add('movie-card');
+            movieCard.setAttribute('id', `${id}`);
+            movieCard.innerHTML = `
+            <img src="${poster_path ? imgUrl + poster_path : 'default-img.jpg'}" />
+              <div class='movie-info'>
+                <h3>${title || name}</h3>
+                <span class='rating'>${vote_average >= 1 ? vote_average.toFixed(1) : '-'}</span>
+              </div>
+            `;
+        
+            main.appendChild(movieCard);
+        });
 }
