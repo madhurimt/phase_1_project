87af1257c9075a93a8787515c5676ec6f055edcb
madhuri maniktahla <madhuri.maniktahla@gmail.com>
2023-09-24 07:21:21 +1000

    Add the Genre Array

--- main.js
+++ main.js
@@ -5,6 +5,30 @@ const moviesUrl = `${baseUrl}discover/movie?sort_by=popularity.desc&api_key=${ap
 const trendingUrl = `${baseUrl}trending/all/day?language=en-US&api_key=${apiKey}`;
 const imgUrl = 'https://image.tmdb.org/t/p/w500';
 
+//Array of Genre Categories
+//For reference -https://www.themoviedb.org/talk/5daf6eb0ae36680011d7e6ee
+const genreArray = [
+  { id: 28, name: 'Action' },
+  { id: 12, name: 'Adventure' },
+  { id: 16, name: 'Animation' },
+  { id: 35, name: 'Comedy' },
+  { id: 80, name: 'Crime' },
+  { id: 99, name: 'Documentary' },
+  { id: 18, name: 'Drama' },
+  { id: 10751, name: 'Family' },
+  { id: 14, name: 'Fantasy' },
+  { id: 36, name: 'History' },
+  { id: 27, name: 'Horror' },
+  { id: 10402, name: 'Music' },
+  { id: 9648, name: 'Mystery' },
+  { id: 10749, name: 'Romance' },
+  { id: 878, name: 'Science Fiction' },
+  { id: 10770, name: 'TV Movie' },
+  { id: 53, name: 'Thriller' },
+  { id: 10752, name: 'War' },
+  { id: 37, name: 'Western' }
+];
+
 const main = document.getElementById('main');
 const trending = document.querySelector('.trending');
 const movies = document.querySelector('.movies');
